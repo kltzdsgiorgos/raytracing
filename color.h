938ebc6f85979429b82c89f4ed684e36266b9302
Giorgos <giorgoskltzds@gmail.com>
2026-04-22 16:43:41 +0300

    creating the .ppm file correctly

--- color.h
+++ color.h
@@ -12,11 +12,10 @@ void write_color(std::ostream& out, const color& pixel_color) {
     auto g = pixel_color.y();
     auto b = pixel_color.z();
 
-    // Translate the [0,1] component valeus to the byte range [0,255].
-
-    int rbyte = int(255.99 * r);
-    int gbyte = int(255.99 * g);
-    int bbyte = int(255.99 * b);
+    // Translate the [0,1] component values to the byte range [0,255].
+    int rbyte = int(255.999 * r);
+    int gbyte = int(255.999 * g);
+    int bbyte = int(255.999 * b);
 
     // Write out the pixel color components.
     out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
